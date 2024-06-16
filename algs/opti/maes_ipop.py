3c4e14d6b70e1dd2b0bfcf77a865cd1af01a49b8
jarnach <jarnach@agora.pl>
2024-06-17 00:36:40 +0200

    ma es with pseudocode + ma es IPOP with pseudocode

--- algs/opti/maes_ipop.py
+++ algs/opti/maes_ipop.py
@@ -2,7 +2,7 @@ import numpy as np
 from .optimizer import *
 
 
-class CMAESM(Optimizer):
+class CMAESM_IPOP(Optimizer):
     """
     Covariance Matrix Adaptation Evolution Strategy with Mirrored Sampling
 
@@ -39,90 +39,129 @@ class CMAESM(Optimizer):
                 Zainicjuj ponownie y, σ, s, M
 
         Until(termination condition(s) fulfilled)
-
     """
-    def __init__(self, func, count):
+
+    def __init__(self, func, count, maxgen, tol=1e-12, cov_tol=1e14):
         super().__init__(func, count)
         self.nn = self.f.D
+        self.maxgen = maxgen
+        self.tol = tol
+        self.cov_tol = cov_tol
 
+        # Inicjalizacja rozmiaru populacji
+        self.init_population_size()
+
+    def init_population_size(self):
         # (M1) Initialize(y(0), σ(0), g := 0, s(0) := 0, M(0) := I)
-        # Initial solution
+        # Początkowe rozwiązanie
         self.xx = np.random.random(self.nn)
-        # Initial mean
+        # Początkowa średnia
         self.xmean = np.copy(self.xx)
-        # Initial step size
+        # Początkowy rozmiar kroku
         self.sigma = 1
 
         # (M2) λ = 4 + int(3 * np.log(nn))
-        # Population size
+        # Rozmiar populacji
         self.lam = 4 + int(3 * np.log(self.nn))
         # (M2) μ = int(λ / 2)
-        # Number of parents/selected points
+        # Liczba rodziców/wybranych punktów
         self.mu = int(self.lam / 2)
-        # (M2) Compute weights and mueff
-        # Weights for recombination
+        # (M2) Obliczanie wag i mueff
+        # Wagi do rekombinacji
         self.weights = np.array([np.log(self.mu + 0.5) - np.log(i + 1) for i in range(self.mu)])
-        # Normalized weights
+        # Normalizacja wag
         self.weights = np.array([w / np.sum(self.weights) for w in self.weights])
-        # Variance-effectiveness of sum w_i x_i
+        # Wariancja efektywna sumy w_i x_i
         self.mueff = 1 / np.sum(np.power(w, 2) for w in self.weights)
 
-        # (M2) cs, c1, cmu parameters
-        # Time constant for cumulation for sigma control
+        # (M2) Parametry cs, c1, cmu
+        # Stała czasowa dla kumulacji do kontroli sigma
         self.cs = (self.mueff + 2) / (self.nn + self.mueff + 5)
-        # Learning rate for rank-one update
+        # Współczynnik uczenia dla aktualizacji rzędu jeden
         self.c1 = 2 / ((self.nn + 1.3) ** 2 + self.mueff)
-        # Learning rate for rank-mu update
+        # Współczynnik uczenia dla aktualizacji rzędu mu
         self.cmu = min([1 - self.c1, 2 * (self.mueff - 2 + 1 / self.mueff) / ((self.nn + 2) ** 2 + self.mueff)])
 
-        # Path for sigma
+        # Ścieżka dla sigma
         self.ps = np.zeros(self.nn)
-        # Identity matrix for the covariance matrix
+        # Macierz jednostkowa dla macierzy kowariancji
         self.M = np.eye(self.nn)
 
+    def check_stagnation(self, fitvals, generation):
+        """
+        Heurystyki do wykrywania stagnacji
+        """
+        # Heurystyka 1: Wszystkie wartości funkcji celu są NaN lub nieskończone
+        if np.all(np.isnan(fitvals)) or np.all(np.isinf(fitvals)):
+            return True
+
+        # Heurystyka 2: Zbyt wysokie wartości macierzy kowariancji
+        if np.any(np.abs(self.M) > self.cov_tol):
+            return True
+
+        # Heurystyka 3: Brak postępu w optymalizacji
+        if generation > 10:
+            recent_fits = fitvals[-10:]
+            if np.ptp(recent_fits) < self.tol:
+                return True
+
+        # Heurystyka 4: Zbyt małe odchylenie standardowe
+        if np.all(self.sigma * np.sqrt(np.diag(self.M)) < self.tol):
+            return True
+
+        # Heurystyka 5: Dodanie wektora odchyleń standardowych w kierunku głównej osi nie zmienia średniej
+        if np.allclose(self.xmean + self.sigma * self.ps, self.xmean, atol=self.tol):
+            return True
+
+        # Heurystyka 6: Dodanie ułamka odchylenia standardowego w każdej współrzędnej nie zmienia średniej
+        if np.allclose(self.xmean + self.sigma * np.sqrt(np.diag(self.M)) / 2, self.xmean, atol=self.tol):
+            return True
+
+        return False
+
     def step(self):
-        # (M3) Repeat For l := 1 To λ
-        # Initialize population
+        # (M3) Powtarzaj dla l := 1 do λ
+        # Inicjalizacja populacji
         pop = np.zeros((self.lam, self.nn))
-        # Initialize random vectors
+        # Inicjalizacja wektorów losowych
         z = np.zeros((self.lam, self.nn))
-        # Initialize transformed vectors
+        # Inicjalizacja przekształconych wektorów
         d = np.zeros((self.lam, self.nn))
         for i in range(self.lam):
             # (M4) z_l(g) := N(0, I)
-            # Sample from normal distribution
+            # Próba z rozkładu normalnego
             z[i] = np.random.normal(0, 1, self.nn)
             # (M5) d_l(g) := M(g) z_l(g)
-            # Transform to the search space
+            # Przekształcenie do przestrzeni poszukiwań
             d[i] = np.dot(self.M, z[i])
             # (M6) y_l(g) := y(g) + σ(g) d_l(g)
-            # Create new population member
+            # Utworzenie nowego członka populacji
             pop[i] = self.xmean + self.sigma * d[i]
 
-        # (M8) SortOffspringPopulation
-        # Evaluate fitness
+        # (M8) Sortowanie populacji potomnej
+        # Ocena dopasowania
         fitvals = np.zeros(self.lam)
         for i in range(self.lam):
             fitvals[i] = self.f.evaluate(pop[i])
-        # Sort population by fitness
+        # Sortowanie populacji według dopasowania
         argx = np.argsort(fitvals)
-        # Check for the best solution
+        # Sprawdzenie najlepszego rozwiązania
         if fitvals[argx[0]] < self.opti_f:
             self.opti_x = pop[argx[0]]
             self.opti_f = fitvals[argx[0]]
 
         # (M9) y(g+1) := y(g) + σ(g) <d(g)>_w
-        # Update mean
+        # Aktualizacja średniej
         self.xmean = self.xmean + self.sigma * np.sum(self.weights[i] * d[argx[i]] for i in range(self.mu))
 
         # (M10) s(g+1) := (1 - cs)s(g) + √(μeffcs(2 - cs)) <z(g)>_w
-        # Update evolution path
+        # Aktualizacja ścieżki ewolucji
         zz = np.sum(self.weights[i] * z[argx[i]] for i in range(self.mu))
         c = np.sqrt(self.cs * (2 - self.cs) * self.mueff)
         self.ps = (1 - self.cs) * self.ps + c * zz
 
         # (M11) M(g+1) := M(g)[I + c1/2 ((s(g+1)(s(g+1))^T) - I) + cmu/2 ( <z(g)(z(g))^T>_w - I )]
-        # Update covariance matrix
+        # Aktualizacja macierzy kowariancji
         one = np.eye(self.nn, self.nn)
         part1 = one
         part2o = self.ps.reshape((self.nn, 1))
@@ -137,16 +176,26 @@ class CMAESM(Optimizer):
         self.M = np.dot(self.M, part1 + part2 + part3)
 
         # (M12) σ(g+1) := σ(g) exp[ cs/cσ (||s(g+1)|| / E ||N(0, I)|| - 1) ]
-        # Update step-size
+        # Aktualizacja rozmiaru kroku
         self.sigma *= np.exp((self.cs / 2) * (np.sum(np.power(x, 2) for x in self.ps) / self.nn - 1))
 
+        return fitvals
+
     def run(self):
-        # (M13) g := g + 1
-        for i in range(self.maxgen):
-            self.step()
-            print(i, self.opti_f)
+        generation = 0
+        while generation < self.maxgen:
+            fitvals = self.step()
+            generation += 1
+            if self.check_stagnation(fitvals, generation):
+                print(f"Stagnation detected at generation {generation}. Restarting with increased population size.")
+                self.lam = int(self.lam * 1.5)
+                self.init_population_size()
+            print(generation, self.opti_f)
 
     def output(self):
         return self.opti_f, self.opti_x
 
-# Until(termination condition(s) fulfilled) (M14)
+# Usage example
+# optimizer = CMAESM_IPOP(your_function, count, maxgen)
+# optimizer.run()
+# opti_f, opti_x = optimizer.output()
